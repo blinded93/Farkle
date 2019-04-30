5d350b2d7ea1e0bbc37dff434758576ace9c35ac
Kyle Hughes <blinded93@gmail.com>
2019-04-30 00:03:54 -0700

    add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   validates :username, :email, uniqueness: true, presence: true
   validates :password, length: { in: 8..25 }
 
+  has_many :games, through: :scorecards
+  has_many :scorecards
+  
   def self.find_by_identifier(identifier)
     id_hash = identifier.include?("@") ? { email: identifier } : { username: identifier }
     User.find_by(id_hash)
