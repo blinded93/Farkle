5028787bf84339094b5f62e4e20427ab31fcc5b5
Kyle Hughes <blinded93@gmail.com>
2019-05-07 16:47:02 -0700

    change filename and complete create action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  # include ActionController::Live
+  # before_action :authenticate_request!
+
+  def create
+    user = User.new(user_params)
+    
+    if user.valid?
+      user.save
+      render json: user, status: 200
+    else
+      binding.pry
+      render json: user, status: 400
+    end
+  end
+  
+  def profile
+    binding.pry
+    # response.headers['Content-Type'] = 'text/event-stream'
+    # user = User.find_by_id(params[:id])
+    # # render json: user
+    # name = user.username
+    # 3.times do |n|
+    #   response.stream.write "#{n}...\n\n"
+    #   sleep 2
+    # end
+    # # response.stream.write "#{name}...\n\n"
+  # ensure
+  #   response.stream.close
+  end
+  
+  private
+
+    def user_params
+      params.permit(:username, :email, :password)
+    end
+end
