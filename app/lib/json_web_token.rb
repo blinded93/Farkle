c52a850e3a86c49c2c6cc1da7aa519e58ff35335
Kyle Hughes <blinded93@gmail.com>
2019-05-07 16:47:38 -0700

    add 24 hour exp date to jwt

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,10 +1,14 @@
 class JsonWebToken
   def self.encode(payload)
-    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    exp = { exp: Time.now.to_i + 86400 }
+    exp_payload = payload.merge(exp)
+    JWT.encode(exp_payload, Rails.application.secrets.secret_key_base)
   end
 
   def self.decode(token)
-    return HashWithIndifferentAccess.new(JWT.decode(token, Rails.application.secrets.secret_key_base)[0])
+    return HashWithIndifferentAccess.new(
+      JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+    )
   rescue
     nil
   end
