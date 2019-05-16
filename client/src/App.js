b604eaee4cdc4c4e1bb0328f9fa3d563f75e897f
Kyle Hughes <blinded93@gmail.com>
2019-05-15 18:46:38 -0700

    add profile path and direct / to currentGame

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
-import Games from './containers/Games'
+import CurrentGame from './containers/CurrentGame'
+import Profile from './containers/Profile'
 import Friends from './containers/Friends'
+import Games from './containers/Games'
 import Nav from './containers/Nav'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
+      <Nav />
       <Switch>
-        <Nav />
-        <Route path='/' component={App} />
+        <Route exact path='/' component={CurrentGame} />
+        <Route path='/profile' component={Profile} />
         <Route path='/games' component={Games} />
         <Route path='/friends' component={Friends} />
       </Switch>
