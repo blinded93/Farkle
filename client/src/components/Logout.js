5a8f3129f027d83fff7aeaab98a84aa7fe3704cd
Kyle Hughes <blinded93@gmail.com>
2019-05-15 18:52:27 -0700

    add logout dialog for modal

---
+++ client/src/components/Logout.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { logout } from '../actions/auth'
+import { Button, Modal } from 'react-bootstrap'
+import { connect } from 'react-redux'
+
+const Logout = props => {
+  const { logout, modalClose } = props
+  const logoutAndClose = () => {
+    logout()
+    modalClose()
+  }
+
+  return (
+    <>
+      <Modal.Body>
+        <div className='text-center'>
+          <div className='mb-2'>Are you sure you want to logout?</div>
+          <Button
+            className='mr-1'
+            size='sm'
+            variant='outline-danger'
+            onClick={e => logoutAndClose()}>
+              Logout
+          </Button>
+          <Button
+            className='ml-1'
+            size='sm'
+            variant='outline-dark'
+            onClick={modalClose}>
+              Cancel
+          </Button>
+        </div>
+      </Modal.Body>
+    </>
+  )
+}
+
+export default connect(null, { logout })(Logout)
