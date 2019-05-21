0e19f1246a4e34f616bf38ce0338f189a0589175
Kyle Hughes <blinded93@gmail.com>
2019-05-20 23:23:23 -0700

    change order of associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   validates :username, :email, uniqueness: true, presence: true
   validates :password, length: { in: 8..25 }
 
-  has_many :games, through: :scorecards
   has_many :scorecards
+  has_many :games, through: :scorecards
   
   def self.find_by_identifier(identifier)
     id_hash = identifier.include?("@") ? { email: identifier } : { username: identifier }
