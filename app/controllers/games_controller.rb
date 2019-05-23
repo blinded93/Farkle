89508dd6f16a52d6af206620ffa3b507a05ddaef
Kyle Hughes <blinded93@gmail.com>
2019-05-22 20:56:00 -0700

    add create action for games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,14 @@
 class GamesController < ApplicationController
+  def create
+    game = Game.new(game_params)
+    # binding.pry
+
+    render json: game, status: 200
+  end
+
+  private
+  
+    def game_params
+      params.permit(:user_id)
+    end
 end
