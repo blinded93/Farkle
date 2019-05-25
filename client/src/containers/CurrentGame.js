3df99c339cb2727b695197c8675b5cd5adeb98d0
Kyle Hughes <blinded93@gmail.com>
2019-05-24 21:30:27 -0700

    remove game board to separate component

--- client/src/containers/CurrentGame.js
+++ client/src/containers/CurrentGame.js
@@ -1,57 +1,28 @@
 import React, { Component } from 'react'
-import { Navbar, Button, Container, Card, Accordion, Row, Col } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+import MobileBoard from '../components/game/MobileBoard'
+import DesktopBoard from '../components/game/DesktopBoard'
+import { connect } from 'react-redux'
 
-export default class CurrentGame extends Component {
+class CurrentGame extends Component {
   state = {
+    width: window.innerWidth,
     
   }
-  
+
   render() {
     return (
       <>
         <Container>
-          <Row>
-            <Col xl={2}  lg={2} md={1} xs={0}></Col>
-            <Col className='sm-game-left' xl={1} lg={1} md={2} sm={2} xs={3}>
-              {
-                ['one', 'two', 'three', 'four', 'five', 'six'].map(num => (
-                  <Row>
-                    <i style={{ 'font-size': '500%' }} className={`fas fa-dice-${num}`} />
-                  </Row>
-                ))
-              }
-            </Col>
-            <Col xl={7} lg={7} md={8} sm={8} xs={8}>
-              <Accordion defaultActiveKey="0">
-                <Card>
-                  <Accordion.Toggle as={Card.Header} eventKey="0">
-                    Click me!
-                </Accordion.Toggle>
-                  <Accordion.Collapse eventKey="0">
-                    <Card.Body>Hello! I'm the body</Card.Body>
-                  </Accordion.Collapse>
-                </Card>
-                <Card>
-                  <Accordion.Toggle as={Card.Header} eventKey="1">
-                    Click me!
-                </Accordion.Toggle>
-                  <Accordion.Collapse eventKey="1">
-                    <Card.Body>Hello! I'm another body</Card.Body>
-                  </Accordion.Collapse>
-                </Card>
-              </Accordion>
-            </Col>
-            <Col xs={1}></Col>
-          </Row>
+          {
+            this.state.width < 560
+              ? <MobileBoard height={this.height} />
+              : <DesktopBoard height={this.height} />
+          }
         </Container>
-        <Navbar fixed='bottom'
-          expand='sm'
-          bg='dark'
-          variant='dark'>
-          <Button style={{ width: '45%' }} variant="outline-success">Search</Button>
-          <Button style={{ width: '45%' }} variant="outline-success">Search</Button>
-        </Navbar>
       </>
     )
   }
 }
+
+export default connect()(CurrentGame)
