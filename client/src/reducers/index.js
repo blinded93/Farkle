99114cabf9f660627deb1efab92d53402d3cb37b
Kyle Hughes <blinded93@gmail.com>
2019-05-27 23:47:07 -0700

    add new reducers to combineReducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux'
 import auth from './auth'
 import modal from './modal'
+import currentGame from './game'
+import scorecards from './scorecards'
 
 export default combineReducers({
   auth,
-  modal
+  modal,
+  currentGame,
+  scorecards
 })
