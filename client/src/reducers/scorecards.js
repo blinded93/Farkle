940b13378fb5e94ef81b558b4487a4cb851cd554
Kyle Hughes <blinded93@gmail.com>
2019-05-27 23:46:54 -0700

    create initial state and scorecards from player data

---
+++ client/src/reducers/scorecards.js
@@ -0,0 +1,32 @@
+const initialState = {
+  player1: {
+    name:'Player1',
+    color:'dark',
+    score: 0,
+    scoresPerTurn: []
+  },
+  player2: {
+    name:'Player2',
+    color:'dark',
+    score: 0,
+    scoresPerTurn: []
+  }
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'CREATE_SCORECARDS':
+      const { player1, player2 } = action.players
+
+      return {
+        player1: {
+          ...state.player1, ...player1
+        },
+        player2: {
+          ...state.player2, ...player2
+        }
+      }
+    default:
+      return state
+  }
+}
