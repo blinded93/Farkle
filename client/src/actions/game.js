2807227a707718c83362ee22bccaa553df444aaa
Kyle Hughes <blinded93@gmail.com>
2019-05-27 23:44:41 -0700

    createGame action

---
+++ client/src/actions/game.js
@@ -0,0 +1,35 @@
+import { createScorecards } from './scorecards'
+
+export const createGame = (players, token) => {
+  return dispatch => {
+    if (!!token) {
+      return fetch('/games', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': token
+        },
+        body: JSON.stringify(players)
+      })
+        .then(resp => resp.json())
+        .then(game => {
+          const { scorecards, ...gameData } = game
+
+          dispatch(newGame(gameData))
+          dispatch(createScorecards(players))
+        })
+    } else {
+      dispatch(newGame())
+      dispatch(createScorecards(players))
+    }
+  }
+}
+
+const newGame = gameData => {
+
+  return {
+    type: 'NEW_GAME',
+    gameData
+  }
+}
