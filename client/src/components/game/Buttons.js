d03b73befa3fb05b611052a2793072a6bfd2ede9
Kyle Hughes <blinded93@gmail.com>
2019-06-03 20:21:20 -0700

    connect component to react for event actions

--- client/src/components/game/Buttons.js
+++ client/src/components/game/Buttons.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
+import { connect } from 'react-redux'
+import { rollDice, bankScore } from '../../actions/turn'
 
 const Buttons = props => {
+  const { rollDice, bankScore, dice, selectedDiceIndexes } = props
+  const handleRoll = (dice, indexes) => { rollDice(dice, indexes) }
+  const handleBank = () => { bankScore() }
+
   return (
     <>
-      <Button style={{ width: '47%' }} variant="outline-success">Roll</Button>
-      <Button style={{ width: '47%' }} variant="outline-danger">Bank</Button>
+      <Button
+        onClick={e => handleRoll(dice, selectedDiceIndexes)}
+        style={{ width: '47%' }}
+        variant="outline-success">
+          Roll
+      </Button>
+      <Button
+        onClick={e => handleBank()}
+        style={{ width: '47%' }}
+        variant="outline-danger">
+          Bank
+      </Button>
     </>
   )
 }
 
-export default Buttons
+const mapStateToProps = state => ({
+  dice: state.turn.dice,
+  selectedDiceIndexes: state.turn.selectedDiceIndexes
+})
+
+export default connect(mapStateToProps, { rollDice, bankScore })(Buttons)
