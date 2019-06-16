6eefe58eb9aec79e07ad1dda68d80f662191b000
Kyle Hughes <blinded93@gmail.com>
2019-06-16 16:44:51 -0700

    add rollCount, unsavedDice, totalScore, finalDice and diceState to state

--- client/src/reducers/turn.js
+++ client/src/reducers/turn.js
@@ -1,39 +1,87 @@
 const initialState = {
-  turn: 0,
+  rollCount: 0,
   selectedDiceIndexes: [],
   savedDice: [],
+  unsavedDice: [0, 1, 2, 3, 4, 5],
+  totalScore: 0,
   selectedScore: 0,
-  dice: ['one', 'two', 'three', 'four', 'five', 'six']
+  dice: ['one', 'two', 'three', 'four', 'five', 'six'],
+  finalDice: [],
+  diceState: ['dice-on', 'dice-on', 'dice-on', 'dice-on', 'dice-on', 'dice-on']
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
+
+    case 'FARKLE':
+      return initialState
+      
+    case 'CONTINUE':
+      debugger
+      return {
+        ...initialState,
+        score: state.totalScore,
+        rollCount: state.rollCount
+      }
+
     case 'SELECT_DICE':
+      const withDiceState = state.diceState.map((s, i) => (
+        i === action.diceIndex ? 'dice-off' : s
+      ))
+
       return {
         ...state,
+        diceState: withDiceState,
+        unsavedDice: state.unsavedDice.filter(d => d !== action.diceIndex),
         selectedDiceIndexes: [...state.selectedDiceIndexes, action.diceIndex]
       }
 
     case 'DESELECT_DICE':
-      const dice = state.selectedDiceIndexes.filter(i => i !== action.diceIndex)
+      const withoutDiceState = state.diceState.map((s, i) => (
+        i === action.diceIndex ? 'dice-on' : s
+      ))
+      const dice = state.savedDice.includes(action.diceIndex)
+        ? state.selectedDiceIndexes
+        : state.selectedDiceIndexes.filter(i => i !== action.diceIndex)
 
       return {
         ...state,
+        diceState: withoutDiceState,
+        unsavedDice: [ ...state.unsavedDice, action.diceIndex ],
         selectedDiceIndexes: dice
+      }
+
+    case 'UPDATE_SCORE':
+      return {
+        ...state,
+        selectedScore: action.score
+      
+      }
+      
+    case 'UPDATE_TOTAL':
+      return {
+        ...state,
+        selectedScore: 0,
+        totalScore: state.totalScore + state.selectedScore
       }
 
     case 'SAVE_DICE':
       return {
         ...state,
         selectedDiceIndexes: [],
-        savedDice: [ ...state.savedDice, ...state.selectedDiceIndexes ]
-      }
-
-    case 'ROLL_DICE':
-      return {
-        ...state,
+        savedDice: [ ...state.savedDice, ...state.selectedDiceIndexes ],
+        finalDice: [],
         dice: action.newDice
       }
+
+    case 'UPDATE_FINAL_DICE':
+      return {
+        ...state,
+        finalDice: state.dice
+      }
+
+    case 'INCREMENT_ROLL_COUNT':
+      return { ...state, rollCount: state.rollCount + 1 }
 
     default:
       return state
