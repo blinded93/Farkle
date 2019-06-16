57f1f1cfef862142e5500fbb9db1f2b2e031baba
Kyle Hughes <blinded93@gmail.com>
2019-06-16 16:46:24 -0700

    add functions to access rule logic and roll dice.

--- client/src/logic/game.js
+++ client/src/logic/game.js
@@ -1,3 +1,5 @@
+import { scoreAccumulation } from './rules'
+
 const indexToString = num => ({
   '0': 'one',
   '1': 'two',
@@ -7,6 +9,15 @@ const indexToString = num => ({
   '5': 'six'
 }[num.toString()])
 
+const stringToNum = string => ({
+  'one': 1,
+  'two': 2,
+  'three': 3,
+  'four': 4,
+  'five': 5,
+  'six': 6
+}[string])
+
 const randomNum = () => Math.round(0 + Math.random() * 5)
 
 export const rollTheDice = (dice, selectedIndexes) => (
@@ -14,6 +25,12 @@ export const rollTheDice = (dice, selectedIndexes) => (
     selectedIndexes && selectedIndexes.includes(i)
       ? d
       : indexToString(randomNum())
-    )
-  )
+  ))
 )
+
+export const checkForScore = dice => {
+  const amounts = { one: 0, two: 0, three: 0, four: 0, five: 0, six: 0 }
+  dice.forEach(d => amounts[d] += 1)
+
+  return scoreAccumulation(amounts)
+}
