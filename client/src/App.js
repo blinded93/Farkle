8268a5ab7f7523a3c2018503e5d03b2825675f98
Kyle Hughes <blinded93@gmail.com>
2019-06-16 19:56:33 -0700

    change routes/links

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
 import CurrentGame from './containers/CurrentGame'
-import Profile from './containers/Profile'
-import Friends from './containers/Friends'
+import Rules from './containers/Rules'
 import Games from './containers/Games'
 import Nav from './containers/Nav'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
@@ -13,9 +12,8 @@ function App() {
       <Nav />
       <Switch>
         <Route exact path='/' component={CurrentGame} />
-        <Route path='/profile' component={Profile} />
+        <Route path='/rules' component={Rules} />
         <Route path='/games' component={Games} />
-        <Route path='/friends' component={Friends} />
       </Switch>
     </Router>
   );
