3bd70d4b43d937dbc40034d6db8450343380d5ce
Kyle Hughes <blinded93@gmail.com>
2019-06-23 18:48:57 -0700

    add index and destroy actions to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,27 @@
 class GamesController < ApplicationController
   before_action :authenticate_request!
+  include Pagy::Backend
+
+  def index
+    pagy, games = pagy(Game.for(current_user.id), items: 1)
+
+    render json: {
+      games: games.map { |g| GameSerializer.new(g) },
+      pagy: pagy
+      }, status: 200
+  end
 
   def create
     game = Game.create_with_scorecards(current_user, player_params)
-    
+    binding.pry
     render json: game, status: 200
+  end
+
+  def destroy
+    game = current_user.games.find_by(id: (params[:id]))
+    status = game ? 200 : 404
+
+    render json: game.destroy, status: status
   end
 
   private
