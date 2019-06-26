d2d502554533457faba1b85a8e3079aee8f2c609
Kyle Hughes <blinded93@gmail.com>
2019-06-25 21:06:14 -0700

    add action creator to change scorecards when opening a game

--- client/src/actions/scorecards.js
+++ client/src/actions/scorecards.js
@@ -2,3 +2,8 @@ export const createScorecards = players => ({
   type: 'CREATE_SCORECARDS',
   players
 })
+
+export const updateScorecards = scorecards => ({
+  type: 'UPDATE_SCORECARDS',
+  scorecards
+})
