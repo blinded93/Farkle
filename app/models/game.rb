b7b349848a28d955418d30c52eb4c27b6ee60e56
Kyle Hughes <blinded93@gmail.com>
2019-06-25 21:01:46 -0700

    change order to desc

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ApplicationRecord
   has_many :scorecards, dependent: :destroy
   
-  scope :for, -> (id) { where(user_id: id) }
+  scope :for, -> (id) { where(user_id: id).order(id: :desc) }
 
   def self.create_with_scorecards(user, players)
     Game.create(user_id: user.id).tap do |game|
