17649cfd9b6d0ee989db0593a44217d79bdc2a27
Kyle Hughes <blinded93@gmail.com>
2019-07-11 18:03:03 -0700

    add update action to scorecards controller

--- app/controllers/scorecards_controller.rb
+++ app/controllers/scorecards_controller.rb
@@ -1,3 +1,14 @@
 class ScorecardsController < ApplicationController
-  :name, :score, :scores_per_turn, :user_id, :game_id
+  before_action :authenticate_request!
+
+  def update
+    scorecard = Scorecard.find_by(id: params[:id])
+    
+    scorecard.score += params[:score].to_i
+    scorecard.scores_per_turn << params[:score]
+    scorecard.save
+    scorecard.game.change_player(params, scorecard)
+
+    render json: scorecard, status: 200
+  end
 end
