11ec02197a83d872ae354856a314793b9e1848c8
Kyle Hughes <blinded93@gmail.com>
2019-07-21 15:47:56 -0700

    change from table to row/col flexbox design for better layout

--- client/src/containers/Rules.js
+++ client/src/containers/Rules.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Table, Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 
 class Rules extends Component {
   
@@ -8,97 +8,107 @@ class Rules extends Component {
     return (
       <>
         <Container>
+          <div className='pb-2 text-center'>
+            <u className='h2'>Rules</u>
+          </div>
           <Row>
-            <Col md={1} lg={1} xl={1}  />
             <Col>
-              <Table borderless size='sm'>
-                <tbody>
-                  <tr>
-                    <td className='text-right'>
-                      <i class="rules-dice fas fa-dice-five" /> FIVES = 50 points  &nbsp;
-                    </td>
-                    <td className='pl-4'>
-                      <i class="rules-dice fas fa-dice-one" /> ONES = 100 points
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className='text-right pr-3'>
-                      <i class="rules-dice fas fa-dice-one" />&nbsp;
-                      <i class="rules-dice fas fa-dice-one" />&nbsp;
-                      <i class="rules-dice fas fa-dice-one" />&nbsp;
-                      = 300 pts.
-                    </td>
-                    <td className='border-left pl-3'>
-                      4 of any number = 1,000 pts.
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className='text-right pr-3'>
-                      <i class="rules-dice fas fa-dice-two" />&nbsp;
-                      <i class="rules-dice fas fa-dice-two" />&nbsp;
-                      <i class="rules-dice fas fa-dice-two" />&nbsp;
-                      = 200 pts.
-                    </td>
-                    <td className='border-left pl-3'>
-                      5 of any number = 2,000 pts.
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className='text-right pr-3'>
-                      <i class="rules-dice fas fa-dice-three" />&nbsp;
-                      <i class="rules-dice fas fa-dice-three" />&nbsp;
-                      <i class="rules-dice fas fa-dice-three" />&nbsp;
-                      = 300 pts.
-                    </td>
-                    <td className='border-left pl-3'>
-                      6 of any number = 3,000 pts.
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className='text-right pr-3'>
-                      <i class="rules-dice fas fa-dice-four" />&nbsp;
-                      <i class="rules-dice fas fa-dice-four" />&nbsp;
-                      <i class="rules-dice fas fa-dice-four" />&nbsp;
-                      = 400 pts.
-                    </td>
-                    <td className='border-left pl-3'>
-                      1-6 straight = 1,500 pts.
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className='text-right pr-3'>
-                      <i class="rules-dice fas fa-dice-five" />&nbsp;
-                      <i class="rules-dice fas fa-dice-five" />&nbsp;
-                      <i class="rules-dice fas fa-dice-five" />&nbsp;
-                      = 500 pts.
-                    </td>
-                    <td className='border-left pl-3'>
-                      Three pairs = 1,500 pts.
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className='text-right pr-3'>
-                      <i class="rules-dice fas fa-dice-six" />&nbsp;
-                      <i class="rules-dice fas fa-dice-six" />&nbsp;
-                      <i class="rules-dice fas fa-dice-six" />&nbsp;
-                      = 600 pts.
-                    </td>
-                    <td className='border-left pl-3'>
-                      Four of any number<br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                      with a pair = 1,500 pts.
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                    </td>
-                    <td className='border-left pl-3'>
-                      Two triplets = 2,500 pts.
-                    </td>
-                  </tr>
-                </tbody>
-              </Table>
+              <Row>
+                <Col>
+                  <div className='pb-2 text-center'>
+                    <b className='h4'>Scoring</b>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col className='text-right'>
+                  <i class="rules-dice fas fa-dice-five" /> FIVES = 50 points  &nbsp;
+                </Col>
+                <Col className='pl-4'>
+                  <i class="rules-dice fas fa-dice-one" /> ONES = 100 points
+                </Col>
+              </Row>
+              <Row>
+                <Col className='text-right pr-3'>
+                  <i class="rules-dice fas fa-dice-one" />&nbsp;
+                  <i class="rules-dice fas fa-dice-one" />&nbsp;
+                  <i class="rules-dice fas fa-dice-one" />&nbsp;
+                  = 300 pts.
+                </Col>
+                <Col className='border-left pl-3'>
+                  4 of any number = 1,000 pts.
+                </Col>
+              </Row>
+              <Row>
+                <Col className='text-right pr-3'>
+                  <i class="rules-dice fas fa-dice-two" />&nbsp;
+                  <i class="rules-dice fas fa-dice-two" />&nbsp;
+                  <i class="rules-dice fas fa-dice-two" />&nbsp;
+                  = 200 pts.
+                </Col>
+                <Col className='border-left pl-3'>
+                  5 of any number = 2,000 pts.
+                </Col>
+              </Row>
+              <Row>
+                <Col className='text-right pr-3'>
+                  <i class="rules-dice fas fa-dice-three" />&nbsp;
+                  <i class="rules-dice fas fa-dice-three" />&nbsp;
+                  <i class="rules-dice fas fa-dice-three" />&nbsp;
+                  = 300 pts.
+                </Col>
+                <Col className='border-left pl-3'>
+                  6 of any number = 3,000 pts.
+                </Col>
+              </Row>
+              <Row>
+                <Col className='text-right pr-3'>
+                  <i class="rules-dice fas fa-dice-four" />&nbsp;
+                  <i class="rules-dice fas fa-dice-four" />&nbsp;
+                  <i class="rules-dice fas fa-dice-four" />&nbsp;
+                  = 400 pts.
+                </Col>
+                <Col className='border-left pl-3'>
+                  1-6 straight = 1,500 pts.
+                </Col>
+              </Row>
+              <Row>
+                <Col className='text-right pr-3'>
+                  <i class="rules-dice fas fa-dice-five" />&nbsp;
+                  <i class="rules-dice fas fa-dice-five" />&nbsp;
+                  <i class="rules-dice fas fa-dice-five" />&nbsp;
+                  = 500 pts.
+                </Col>
+                <Col className='border-left pl-3'>
+                  Three pairs = 1,500 pts.
+                </Col>
+              </Row>
+              <Row>
+                <Col className='text-right pr-3'>
+                  <i class="rules-dice fas fa-dice-six" />&nbsp;
+                  <i class="rules-dice fas fa-dice-six" />&nbsp;
+                  <i class="rules-dice fas fa-dice-six" />&nbsp;
+                  = 600 pts.
+                </Col>
+                <Col className='border-left pl-3'>
+                  Four of any number<br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  with a pair = 1,500 pts.
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                </Col>
+                <Col className='border-left pl-3'>
+                  Two triplets = 2,500 pts.
+                </Col>
+              </Row>
             </Col>
-            <Col md={1} lg={1} xl={1} />
+            <Col>
+             <h4>Objective</h4>
+             <p>To score points through systematic rolling of the dice.</p>
+             <h5>How to Play</h5>
+             <p>The first player rolls 6 dice and selects (sets aside) any scoring dice that appear. Players can continue to roll the remaining dice for more points, setting aside a minimum of 1 scoring dice each turn. 500 points must be collected before points can be added to scorecard (banked). If a roll does not contain any scoring dice, the player has 'FARKLED' and loses points and turn.</p>
+            </Col>
           </Row>
         </Container>
       </>
