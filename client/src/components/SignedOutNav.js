3e9a04f4e4700898e59f9a62f7d31d2c78e831e8
Kyle Hughes <blinded93@gmail.com>
2019-07-21 15:43:28 -0700

    add rules link to signed out menu

--- client/src/components/SignedOutNav.js
+++ client/src/components/SignedOutNav.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Nav } from 'react-bootstrap'
-import { IndexLinkContainer } from 'react-router-bootstrap'
+import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap'
 
 const SignedOutNav = ({ modalOpen }) => {
   return (
     <>
+      <LinkContainer to='rules'>
+        <Nav.Link>
+          Rules
+        </Nav.Link>
+      </LinkContainer>
+      <span style={{'font-size': 25, color: 'dark-brown'}}>|</span>
       <IndexLinkContainer to='/'>
         <Nav.Link onClick={e => modalOpen('Login')}>
             Login
